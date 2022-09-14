5f14c6c3eb7b19981238ed7ce261d48eeb5cfc6b
EternalII <Lior_acolyte@walla.com>
2022-09-14 18:43:49 +0300

    Update attendance.py

--- Ex3/attendance.py
+++ Ex3/attendance.py
@@ -51,25 +51,77 @@ filesList, filepList = getListDirFiles(search)
 remDups(filepList) #removes duplicates
 pth, newdir = createDir("Attendance") #pth is path to csv_files/Attendance
 
+def getKey():
+    df_key = pd.DataFrame()
+    df_sample = pd.read_csv('participant-20220823132400.csv', encoding="UTF-16LE", sep="\t")
+    df_sample = df_sample.drop_duplicates(subset=['Attendee Email'])
+    
+    df_sample = df_sample.set_index('Attendee Email')
+    df_key['Attendee Email'] = df_sample.index
+    df_key = df_key.set_index('Attendee Email')
+    
+    df_key['Name'] = df_sample['Name']
+    
+    return df_key
+
+df_key = getKey() #starts a new table which will also stores our key email values from the sample
+df_key = df_key.sort_values(by=['Name'])
+
+def pullRelData(df)
+	new_row = df.loc[10]
+	df = df.append(new_row,ignore_index=True)
+
+	df.at[62, "Name"] = "TEST"
+	df.at[62, "Attendee Email"] = "TEST@test.com"
+	df.at[62, "Attendance Duration"] = "0 mins"
+	
+	df[['Attendance Time','M']] = df['Attendance Duration'].str.split(' ', expand=True) #splits duration to int time and char min
+
+	df[['Date','Start Hour']] = df['Meeting Start Time'].str.split(' ',expand=True)
+	df[['End Date',"End Hour"]] = df['Meeting End Time'].str.split(' ',expand=True)
+
+	df[['sHour', 'sMinute', 'sSecond']] = df['Start Hour'].str.split(':',expand=True)
+	df[['eHour', 'eMinute', 'eSecond']] = df['End Hour'].str.split(':',expand=True)
+	df = df.drop(columns=["Meeting Start Time","Meeting End Time"])
+
+	df["sHour"] = df["sHour"].str.extract('(\d+)', expand=False).astype(int)
+	df["sMinute"] = df["sMinute"].str.extract('(\d+)', expand=False).astype(int)
+	df["eHour"] = df["eHour"].str.extract('(\d+)', expand=False).astype(int)
+	df["eMinute"] = df["eMinute"].str.extract('(\d+)', expand=False).astype(int)
+
+	df["Hour_diff"] = df["eHour"]-df["sHour"]
+	df["Min_diff"] = df["eMinute"]-df["sMinute"]
+	df["Session Duration"] = df["Hour_diff"]*60+df["Min_diff"]
+
+	#Email is key. Combines emails and sums them by time.
+	dropList = df.columns[:]
+	dropList = dropList.drop(['Name', 'Attendee Email', 'Attendance Time', 'Session Duration', 'Date'])
+	
+	df = df.drop(columns=dropList)
+	df["Date"] = df["Date"].str.extract('(\d+-\d+-\d+)', expand=False)
+
+	df["Attendance Time"] = df["Attendance Time"].astype(int)
+	df_grouped = df[['Attendee Email', 'Attendance Time']]
+	df_grouped = df_grouped.groupby('Attendee Email').sum() #groups by email and sums their attendance time
+	df = df.drop_duplicates(subset=['Attendee Email'])
+	df = df.set_index('Attendee Email')
+
+	df = df.sort_values(by=['Name'])
+	df["Attendance Time"] = df_grouped["Attendance Time"]
+	
+	return df
 
 for each in unique_files:
 	df = pd.read_csv(cwd+"/"+unique_files[each], encoding="UTF-16LE", sep="\t")	
+	pullRelData(df)
+	##Attach new data to key data
+	dateName = df["Date"][0]
+	df_key[dateName] = df["Attendance Time"]
 
-	df[['Time','M']] = df['Attendance Duration'].str.split(' ', expand=True)
+sumAllTimes = df_key.iloc[:,1:].sum(axis=1)
+maxTimes = 4*60*(len(df_key.columns)-2)
+df_key["Total Percentage"]  = sumAllTimes/maxTimes
 
-	df[['Date','Hour']] = df['Meeting Start Time'].str.split(' ',expand=True)
 
-	df[['sHour', 'sMinute', 'sSecond']] = df['Hour'].str.split(':',expand=True)
-	dropList = df.columns[:]
-	dropList = dropList.drop(['Name', 'Time'])
-	df["Time"] = df["Time"].astype(int)
-
-	#df_grouped = df.groupby('Attendee Email')["Time"].sum()
-
-	df_grouped = df.drop(columns=dropList)
-
-	df_grouped = df.groupby('Name').sum()
-	df_grouped.sort_values(by=['Name'])
-	
-	df_grouped.to_csv(pth+"/list_"+each)
+df_key.to_csv(pth+"/Attendance.csv")
 
